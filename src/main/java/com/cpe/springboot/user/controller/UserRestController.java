cc0d7fd099401abd5cfc841359e6c5b2acfe8038
bip <bipbip@qsdqs.com>
2021-11-26 16:19:13 +0100

    minor update

--- src/main/java/com/cpe/springboot/user/controller/UserRestController.java
+++ src/main/java/com/cpe/springboot/user/controller/UserRestController.java
@@ -1,9 +1,9 @@
 package com.cpe.springboot.user.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.cpe.springboot.common.tools.DTOMapper;
 import com.cpe.springboot.user.model.UserDTO;
