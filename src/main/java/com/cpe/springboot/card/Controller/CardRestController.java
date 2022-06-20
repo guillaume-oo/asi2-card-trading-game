9e11bb762bd6024978f7c072df401257d8e32b46
bip <bipbip@qsdqs.com>
2022-06-20 11:22:12 +0200

    update API and object return, add swagger doc

--- src/main/java/com/cpe/springboot/card/Controller/CardRestController.java
+++ src/main/java/com/cpe/springboot/card/Controller/CardRestController.java
@@ -4,17 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.cpe.springboot.card.model.CardDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.cpe.springboot.card.model.CardDTO;
 import com.cpe.springboot.card.model.CardModel;
 import com.cpe.springboot.common.tools.DTOMapper;
-import com.cpe.springboot.user.model.UserModel;
 
 //ONLY FOR TEST NEED ALSO TO ALLOW CROOS ORIGIN ON WEB BROWSER SIDE
 @CrossOrigin
 @RestController
+
 public class CardRestController {
 
 	private final CardModelService cardModelService;
@@ -23,7 +29,7 @@ public class CardRestController {
 		this.cardModelService=cardModelService;
 	}
 	
-	@RequestMapping("/cards")
+	@RequestMapping(method=RequestMethod.GET, value="/cards")
 	private List<CardDTO> getAllCards() {
 		List<CardDTO> cLightList=new ArrayList<>();
 		for(CardModel c:cardModelService.getAllCardModel()){
@@ -33,26 +39,26 @@ public class CardRestController {
 
 	}
 	
-	@RequestMapping("/card/{id}")
+	@RequestMapping(method=RequestMethod.GET, value="/card/{id}")
 	private CardDTO getCard(@PathVariable String id) {
 		Optional<CardModel> rcard;
 		rcard= cardModelService.getCard(Integer.valueOf(id));
 		if(rcard.isPresent()) {
 			return DTOMapper.fromCardModelToCardDTO(rcard.get());
 		}
-		return null;
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Card id:"+id+", not found",null);
 
 	}
 	
 	@RequestMapping(method=RequestMethod.POST,value="/card")
-	public void addCard(@RequestBody CardDTO card) {
-		cardModelService.addCard(DTOMapper.fromCardDtoToCardModel(card));
+	public CardDTO addCard(@RequestBody CardDTO card) {
+		return cardModelService.addCard(DTOMapper.fromCardDtoToCardModel(card));
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT,value="/card/{id}")
-	public void updateCard(@RequestBody CardDTO card,@PathVariable String id) {
+	public CardDTO updateCard(@RequestBody CardDTO card,@PathVariable String id) {
 		card.setId(Integer.valueOf(id));
-		cardModelService.updateCard(DTOMapper.fromCardDtoToCardModel(card));
+		 return cardModelService.updateCard(DTOMapper.fromCardDtoToCardModel(card));
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE,value="/card/{id}")
@@ -60,7 +66,7 @@ public class CardRestController {
 		cardModelService.deleteCardModel(Integer.valueOf(id));
 	}
 
-	@RequestMapping("/cards_to_sell")
+	@RequestMapping(method=RequestMethod.GET, value="/cards_to_sell")
 	private List<CardDTO> getCardsToSell() {
 		List<CardDTO> list=new ArrayList<>();
 		for( CardModel c : cardModelService.getAllCardToSell()){
